125a8f42e97ac39d34644a1a8587e0d80d0f81c1
yeleix <yelei.xiao@gmail.com>
2018-03-09 03:09:47 -0500

    Rename shell script
    get-contractors.sh change to get-contractor.sh

--- scripts/contractors/get-contractors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl "http://localhost:4741/contractors/" \
---include \
---request GET \
---header "Authorization: Token token=${TOKEN}" \
---header "Content-Type: application/json" \
-
-echo
